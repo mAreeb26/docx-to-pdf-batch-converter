4613de8cb7beca38841cda7e2db44619453be809
mAreeb <293592612+mAreeb26@users.noreply.github.com>
2026-06-17 11:20:42 +0530

    added option to add convert destination

--- d2p.py
+++ d2p.py
@@ -3,11 +3,15 @@ from pathlib import Path
 
 count=0
 exclude=input('Exclusions(ex-note1,book,project): ')
+output=input('Output path(default-same folder as docx files): ')
 exclusions=[e + '.docx' for e in exclude.split(',')]
 folder=Path(__file__).parent
 for file in folder.glob('*.docx'):
     if file.name not in exclusions:
-        convert(file)
+        if output==None:
+            convert(file)
+        else:
+            convert(file,output + '\\' + file.stem + '.pdf')
         print(file.name + ' ✓')
         count+=1
 print(str(count) + ' files converted!')
